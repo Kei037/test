91e3399eba096e28f5e724f5cf292f39c4b8a04b
junyoung <juneway@hanmail.net>
2024-02-27 16:45:44 +0900

    python study commit

--- part01/q_test.py
+++ part01/q_test.py
@@ -82,5 +82,248 @@ elif age >= 8:
 else:
     print('미취학')
 
+
+scores = int(input('점수를 입력하세요 >>> '))
+if scores >= 90:
+    grade = 'A'
+elif scores >= 80:
+    grade = 'B'
+elif scores >= 70:
+    grade = 'C'
+elif scores >= 60:
+    grade = 'D'
+else:
+    grade = 'F'
+print(f'점수는 {scores}점이고, 학점은 {grade} 학점입니다.')
+print()
+
+num = int(input('정수를 입력하세요 >>> '))
+if num % 3 == 0:
+    print(f'{num}는 3의 배수입니다.')
+else:
+    print(f'{num}는 3의 배수가 아닙니다.')
+print()
+
+num1 = int(input('정수1 입력 >>> '))
+num2 = int(input('정수2 입력 >>> '))
+num3 = int(input('정수3 입력 >>> '))
+
+if num1 > num2 and num1 > num3:
+    big_num = num1
+elif num2 > num1 and num2 > num3:
+    big_num = num2
+elif num3 > num1:
+    big_num = num3
+print(f'가장 큰 수는 {big_num}입니다.')
+
+
+
+# 사용자로 부터 임으의 정수를 입력받아 모두 리스트에 보관
+# 단 사용자가 0을 입력하면 프로그램을 종료. 이 때 입력받은 0은 리스트에 보관하지 않음
+
+my_list = []
+
+n = int(input('정수를 입력하세요(종료는 0입니다.) >>> '))
+
+while n != 0:
+    my_list.append(n)
+    n = int(input('정수를 입력하세요(종료는 0입니다.) >>> '))
+
+print(my_list)
+
+
+
+num_for = int(input('정수를 입력하세요 >>> '))
+if num_for <= -0:
+    print('잘못된 입력입니다.')
+else:
+    n = 1
+    while n <= num_for:
+     print(f'{n}번째 Hello')
+     n += 1
+
+
+money = int(input('자판기에 얼마를 넣을까요? >>> '))
+n = 1
+while money >= 300:
+    money -= 300
+    print(f'커피{n}잔, 잔돈{money}')
+    n += 1
+
+numbers = set()
+while len(numbers) < 5:
+    s_num = int(input('0 ~ 9 사이 정수를 입력하세요 >>> '))
+    numbers.add(s_num)
+print('모두 입력되었습니다.')
+print(f'입력된 값은 {numbers}입니다.')
+
+
+
+# 문자열로 비밀번호를 입력 받아 숫자와 문자가 모두 포함이 되었는지 확인
+pwd = input('비밀번호를 입력하세요 >>> ')
+
+ch_count = 0  # 문자의 개수 저장
+num_count = 0  # 숫자의 개수 저장
+
+for ch in pwd:  # 반복가능객체가 문자열
+    if ch.isalpha():  # ch가 문자인 경우 True를 반환
+        ch_count += 1
+    elif ch.isnumeric():  # ch가 숫자인 경우 True를 반환
+        num_count += 1
+
+if ch_count > 0 and num_count > 0:
+    print('가능한 비밀번호입니다.')
+else:
+    print('불가능한 비밀번호입니다.')
+
 """
+"""
+
+dan = int(input('출력할 구구단을 입력하세요 >>> '))
+for n in range(1, 10):
+    print(f'{dan} x {n} = {dan * n}')
+
+"""
+"""
+for num in range(5, 0, -1):
+    print(num)
+
+f_num = int(input('임의의 양수를 입력하세요 >>> '))
+sum = 0;
+for n in range(0, f_num + 1):
+    sum += n
+print(f'1부터 {f_num}사이의 모든 정수의 합계는 {sum}입니다.')
+
+fruit_list = list()
+s_num = int(input('몇 개의 과일을 보관할까요? >>> '))
+for n in range(1, s_num + 1):
+    fruit = input(f'{n}번재 과일을 입력하세요 >>> ')
+    fruit_list.append(fruit)
+print(f'입력 받은 과일들은 {fruit_list}입니다.')
+
+# break
+while True:
+    city = input('대한민국의 수도는 어디인가요? >>> ')
+    city = city.strip()  # 문자열의 양쪽끝에서 공백을 없앰
+    if city == '서울' or city == 'seoul' or city == 'SEOUL':
+        print('정답입니다.')
+        break
+    else:
+        print('오답입니다. 다시 시도하세요.')
+        
+
+hobbies = []
+while True:
+    hobby = input('취미를 입력하세요(종료는 그냥 q) >> ')
+    if hobby == 'q':
+        print('입력된 취미가 모두 저장되었습니다.')
+        break
+    else:
+        hobbies.append(hobby)
+
+    if len(hobbies) >= 5:
+        print('5개 입력!')
+        break
+print(hobbies)
+
+fruits = ['사과', '감귤']
+count = 3
+
+while count > 0:
+    fruit = input('어떤 과일을 저장할까요? >>> ')
+
+    if fruit in fruits:  # fruits 리스트에 fruit 변수에 있는 값이 있다면
+        print('동일한 과일이 있습니다.')
+        continue  # while문의 시작 지점으로 돌아가서 다시 과일 이름을 입력
+
+    fruits.append(fruit)
+    count -= 1
+    print(f'입력이 {count}번 남았습니다.')
+print(f'5개 과일은 {fruits}입니다.')
+"""
+"""
+money = 10000
+while money > 0:
+    print(f'현재 {money}원이 있습니다.')
+    amount = int(input('사용할 금액 입력 >>> '))
+    if amount <= 0:
+        print('0이하의 금액은 사용할 수 없습니다.')
+        continue
+    if amount > money:
+        print(f'{amount - money}원이 무족합니다.')
+        continue
+    money -= amount
+print(f'현재 {money}원이 있습니다.')
+print()
+
+flag = True
+while flag:
+    score = int(input('이번 영화의 평점을 입력하세요 >>> '))
+    if 0 >= score or score >= 6:
+        print('평점은 1~5 사이만 입력할 수 있습니다.')
+        continue
+    print('평점: {}'.format(score * '*'))
+    flag = False
+print()
+
+while True:
+    score = int(input('이번 영화의 평점을 입력하세요 >>> '))
+    if 1 <= score <= 5:
+        print(f'평점: {"*" * score}')
+        break
+    print('평점은 1~5 사이만 입력할 수 있습니다.')
+print()
+
+password = 'qwerty'
+count = 5
+
+while True:
+    ipw = input('비밀번호를 입력하세요 >>> ')
+    count -= 1
+    if ipw == password:
+        print('비밀번호를 맞혔습니다.')
+        break
+    if count == 0:
+        print('비밀번호 입력 횟수를 초과했습니다.')
+        break
+
+"""
+
+"""
+print('점수를 입력하세요.')
+print('더 이상 입력할 점수가 없으면 음수를 아무거나 입력하세요.')
+scores = 0
+exam = []
+while True:
+    scores = int(input('점수 입력 >>> '))
+    if scores < 0:
+        break
+    exam.append(scores)
+print(f'평균 = {sum(exam) / len(exam)}, 최대 = {max(exam)}, 최소 = {min(exam)}')
+
+"""
+
+
+months_eng = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+              'August', 'September', 'October', 'November', 'December']
+months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+# range() 사용
+for idx in range(len(months_eng)):
+    print(f'{months_eng[idx]} = {months[idx]}')
+print()
+
+# enumerate() 사용
+for idx, v in enumerate(months_eng):
+    print(f'{v} = {months[idx]}')
+print()
+
+# dict 사용
+months_dict = dict()
+for i, v in enumerate(months_eng):
+    months_dict.update({v: months[i]})
+
+for k, v in months_dict.items():
+    print(f'{k} = {v}일')
+
 
--- part01/test02.py
+++ part01/test02.py
@@ -159,3 +159,384 @@ elif important >= 50:
     print('중')
 else:
     print('하')
+
+
+# while 문
+# 특정 조건을 만족하는 동안 반복해서 수행해야 하는 코드를 작성할 때 사용
+# while 조건식 :
+#   반복실행문
+
+n = 1
+while n <= 10:
+    print(n)
+    n += 1
+print(n)
+
+n = 100
+while n >= 50:
+    if n % 2 == 0:
+        print(n)
+    n -= 1
+
+n = 100
+while n >= 50:
+    print(n)
+    n -= 2
+
+# 구구단 2단 부터 9단 까지 출럭
+# 단 앞에 제목, 마지막에 구분선을 넣어 볼 것
+
+dan = 2
+while dan <= 9:
+    n = 1
+    print(f'{dan}단')
+    while n <= 9:
+        print(f'{dan} x {n} = {dan * n}')
+        n += 1
+    print('==========')
+    dan += 1
+
+
+# for 문
+# 값의 범위나 횟수가 정해져 있을 때 사용하면 편리한 반복문
+# 1) 특정 횟수 반복
+# 2) 데이터 순회
+
+# for 변수 in 반복개능객체 :
+# 반복 실행문
+
+for n in [2, 4, 6] :  # 반복될 때 마다 순서대로 전달
+    print(n)
+
+# 반복가능객체
+# 1. 시퀀스 sequence 자료형 : 순서를 가지고 있는 자료형
+# 문자열, 리스트, 튜플, range
+# 2. 비시퀸스 non-sequence 자료형 : 순서를 가지고 있지 않은 자료형
+# 세트, 딕셔너리
+
+# 1. 문자열
+# for 문자 in 문자열 :
+#   반복실행문
+
+for ch in 'hello':
+    print(ch)
+
+# 2. 리스트
+# for 요소 in [리스트]:
+#   반복문
+
+li = ['가위', '바위', '보']
+for i, v in enumerate(li): # i:index, v:요소의 값 value
+    print(f'인덱스: {i} Value값: {v}')
+
+# 리스트 내포
+# 리스트 생성할 때 for문을 유용하게 사용할 수 있음
+# 기본형식
+# 리스트 = [표현식 for 변수 in 반복가능객체]
+li = [n * 2 for n in [1, 2, 3]]
+print(li)
+
+# 조건에 맞는 데이터만 추출 가능
+# 리스트 = [표현식 for 변수 in 반복가능객체 if 조건식]
+li = [n * 2 for n in [1, 2, 3, 4, 5]]
+print(li)
+
+li = [n * 2 for n in [1, 2, 3, 4, 5] if n % 2 == 1] # 1, 3, 5 만 추출
+print(li)
+
+li = []
+for n in [1, 2, 3, 4, 5]:
+    if n % 2 == 1:
+        li.append(n * 2)
+print(li)
+
+# 3. 튜플
+# 기본형식
+# for 요소 in (튜플):
+#   반복실행문
+
+four_seasons = ('Spring', 'Summer', 'Autumn', 'Winter')
+for season in four_seasons:
+    print(season)
+
+# 4. range() 함수
+# '정수 범위'를 만들어 낼 때 유용한 함수. 일정 횟수 반복시에 많이 사용
+# 기본 구조
+# range(초깃값, 종료값, 증감값)
+
+# 특징
+# 1. 초깃값부터 종료값 전까지 숫자(n)들의 컬렉션을 만듬 (초깃값 <= n < 종료값)
+# 2. 초깃값을 생략하면 0부터 시작
+# 3. 종료값은 생략할 수 없음
+# 4. 증감값을 생략하면 1씩 증가
+
+# 예
+# range(5) : 0, 1, 2, 3, 4
+# range(1, 11) : 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+# range(1, 10, 2) : 1, 3, 5, 7, 9
+
+print()
+print(list(range(1, 6)))
+print(tuple(range(1, 6)))
+
+print()
+for n in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+    print(n)
+
+# 아래와 같이 변경 가능
+for n in range(1, 11):
+    print(n)
+
+# range() 함수를 이용해 생성한 값을 사용하지 않는 경우
+for n in range(10):
+    print('hello')
+
+dan = 2
+for dan in range(2, 10):
+    print(f'{dan}단')
+    for n in range(1, 10):
+        print(f'{dan} x {n} = {dan * n}')
+    print('==========')
+
+
+# 세트 set
+# 비시퀀스 자료형이기 때문에 저장된 요소들 사이에 순서가 없음
+# for 요소 in {세트}:
+#   반복실행문
+
+for item in {'가위', '바위', '보'}:
+    print(item)
+
+
+# 딕셔너리
+# key와 value의 조합이라 다른 자료형과 다른 방식으로 사용을 함
+# 키만 출력
+print()
+person = {'name': '에밀리', 'age': 20}
+for item in person:
+    print(item)
+
+# value 출력
+for key, value in person.items():
+    print(f'{key} : {value}')
+
+for key in person:
+    print(person[key])
+
+for key in person:  # get() 메소드를 이용해서 해당 key에 해당하는 value를 가져옴
+    print(person.get(key))
+
+
+# 영어 사전을 딕셔너리 자료형으로 구현하고
+# 영어 사전에 등록된 모든 단어와 그 단어의 의미를 출력
+eng_dict = {
+    'sun': '태양',
+    'moon': '달',
+    'star': '별',
+    'space': '우주'
+}
+for word in eng_dict:
+    print(f'{word}의 뜻: {eng_dict.get(word)}')
+    print(f'{word}의 뜻: {eng_dict[word]}')
+
+
+# break
+n = 1
+while True:
+    print(n)
+    if n == 10:
+        break
+    n += 1
+print(n)
+
+
+# continue 문
+total = 0
+for a in range(1, 101):
+    if a % 3 == 0:
+        continue
+    total += a
+print(total)
+
+# 내장 함수
+# 파이썬 인터프리터에는 항상 사용할 수 있는 많음 함수가 내장되어 있으며,
+# 이를 내장 함수 built-in function 이라고 함
+# 외부에 따로 저장해 둔 모듈에서 불러오는 것이 아니기 때문에 import가 필요없음
+
+# 1. 문자열 내장 함수
+# chr() : 특정 문자의 유니코드 값을 전달하면 해당 유니코드 값을 가진 문자를 반환하는 함수
+# 유니코드의 유효범위 0 ~ 1,114,111 / 아스키코드의 유효 범위 0 ~ 127
+print(chr(48))  # 0
+print(chr(49))  # 1
+print(chr(65))  # A
+print(chr(66))  # B
+print(chr(97))  # a
+print(chr(98))  # b
+
+# ord() : 문자를 전달하면 해당 문자의 유니코드 값을 반환
+print()
+print(ord('A'))  # 65
+print(ord('한'))  # 54620
+
+# eval() : 실행하고자 하는 표현식(expression) 을 문자열로 전달하면 표현식의 결과값을 반환
+print()
+print(eval('100 + 200'))  # 300
+a = 10
+print(eval('a * 5'))  # 50
+print(eval('min(1, 2, 3)'))  # 1
+
+# format() : 전달받은 인수와 포멧 코드에 따라 형식을 갖춘 문자열을 반환
+# 포켓 코드를 전달하지 않으면 str()을 호출한 것과 같음
+print()
+print(format(10000))  # 10000 / str(10000)과 같음
+print(format(10000, '-'))  # 10_000 / 천단위 구분 기호로 밑줄(_)을 사용
+print(format(10000, ','))  # 10,000 / 천단위 구분 기호로 밑줄(,)을 사용
+
+# 2. 숫자 내장 함수
+# abs() : 전달된 인수 (정수 혹은 실수)의 절대값을 반환
+print(abs(10))  # 10
+print(abs(-10))  # 10
+
+# divmod() : 전달된 두 인수를 나누어 몫과 나머지를 한 쌍의 결과로 반환. 몫과 나머지는 하나의 튜플(tuple)로 반환
+print(divmod(10, 3))  # (3, 1)
+print(type(divmod(10, 3)))  # <class 'tuple'>
+
+# float() : 전달된 인수(숫자 또는 문자열)를 실수로 만들어 반환. 전달된 인수가 없는 경우에는 0.0을 반환
+
+# int() : 전달된 인수(숫자 또는 문자열)를 정수로 만들어 반환. 전달된 인수가 없는 경우에는 0을 반환
+
+# max() : 전달된 인수 중 가장 큰 값을 반환
+print()
+print(max(1, 10))
+li = [5, 3, 4, 2, 1]
+print(max(li))
+
+# min() : 전달된 인수 중 가장 작은 값을 반환
+print()
+print(min(1, 10))
+li = [5, 3, 4, 2, 1]
+print(min(li))
+
+# pow() : 전달된 두 인수의 거듭제곱을 반환. 연산자 중에서 거듭 제곱 연산자 ** 와 같음
+print()
+print(pow(10, 2))  # 100
+print(pow(10, 3))  # 1000
+print(pow(10, -2))  # 0.01
+print(pow(10, -3))  # 0.001
+
+# round() : 전달된 인수를 이용해 반올림한 값을 반환
+# 전달된 인수가 하나이면 정수로 반올림함 값을 반환하고, 전달된 인수가 2개이면 두 번째로 전달한 인수만큼 소수점을 남겨둠
+print()
+print(round(1.5))  # 2
+print(round(1.4))  # 1
+print(round(1.55, 1))  # 1.6
+print(round(2.675, 2))  # 2.67 -> 부동 소수점 에러로 인해 2.68이 아니라 2.67이 반환
+print(round(2.6755, 2))  # 2.68
+
+# sum() : 전달된 리스트나 튜플과 같은 반복가능객체의 합계를 반환
+# 숫자가 아닌 값을 전달하면 에러
+print()
+list1 = [1, 2, 3, 4, 5]
+print(sum(list1))
+
+list2 = ['h', 'e', 'l', 'l', 'o']
+# print(sum(list2))
+
+# 3. 시퀀스 내장 함수
+
+# enumerate() : 리스트에 저장된 요소와 해당 요소의 인덱스가 튜플 tuple 형태로 함게 추출
+# for item in enumerate([리스트]) :
+#   반복실행문
+
+for item in enumerate(['가위', '바위', '보']) :
+    print(item)
+# (0, '가위')
+# (1, '바위')
+# (2, '보')
+
+for idx, element in enumerate(['가위', '바위', '보']) :
+    print(idx, ' / ', element)
+# 0  /  가위
+# 1  /  바위
+# 2  /  보
+
+# range() : 전달된 인수값에 따라 시퀀스 자료형의 데이터를 생성
+# 1) range(stop) / 0부터 stop 사이의 모든 정수가 생성. 생성되는 정수를 n이라고 하면 n의 범위는 0 <= n < stop
+# 2) range(start, stop) / start부터 stop 사이의 모든 정수가 생성. 생성되는 정수를 n이라고 하면 n의 범위는 start <= n < stop
+# 3) range(start, stop, step) / start부터 stop 사이의 정수 중에서 step만큼의 차이를 가지고 있는 정수가 생성.
+
+print()
+print(list(range(10))) # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(list(range(1, 11))) # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(list(range(0, 30, 5))) # [0, 5, 10, 15, 20, 25]
+print(list(range(0, 10, 3))) # [0, 3, 6, 9]
+print(list(range(0, -10, -1))) # [0, -1, -2, -3, -4, -5, -6, -7, -8, -9]
+print(list(range(0))) # []
+print(list(range(1, 0))) # []
+
+# len() : 함수에 전달된 객체의 길이(항목 수)를 반환
+print()
+li = ['a', 'b', 'c', 'd']
+print(len(li))
+
+d = {'a': 'apple', 'b': 'banana'}
+print(len(d))
+
+print(len(range(10)))
+print(len(range(1, 10)))
+
+# range() 함수와 리스트의 길이를 구하는 len() 함수를 함게 사용하면 리스트의 인덱스를 생성가능
+seasons = ['봄', '여름', '가을', '겨울']
+seasons_eng = ['spring', 'summer', 'fall', 'winter']
+for idx in range(len(seasons)):
+    print(f'{seasons[idx]} / {seasons_eng[idx]}')
+
+
+
+# sorted() : 전달된 반복가능객체의 오름차순 정렬 결과를 반환
+# reverse=True 옵션을 추가할 경우 내림차순 정렬 결과를 반환
+print()
+my_list2 = ['b', 'c', 'a', 'd']
+print(sorted(my_list2))  # ['a', 'b', 'c', 'd']
+print(sorted(my_list2, reverse=True))  # ['d', 'c', 'b', 'a']
+
+my_list = [6, 3, 1, 2, 5, 4]
+print(sorted(my_list))  # [1, 2, 3, 4, 5, 6]
+print(sorted(my_list, reverse=False))  # [1, 2, 3, 4, 5, 6]
+print(sorted(my_list, reverse=True))  # [6, 5, 4, 3, 2, 1]
+
+print()
+print(sorted(my_list))  # [1, 2, 3, 4, 5, 6]
+print(my_list)  # [6, 3, 1, 2, 5, 4] / 실제로 정렬이 된건 아님
+
+print()
+my_list = sorted(my_list)  # 오름차순 정렬 결과를 덮어쓰기
+print(my_list)  # [1, 2, 3, 4, 5, 6]
+
+# zip() : 전달된 여러 개의 반복가능객체의 각 요소를 튜플로 묶어서 반환
+# 전달된 반복가능객체들의 길이가 서로 다르면 길이가 짧은 반복가능객체 기준으로 동작
+print()
+names = ['james', 'emily', 'amanda']
+scores = [60, 70, 80]
+for student in zip(names, scores):
+    print(student)
+# ('james', 60)
+# ('emily', 70)
+# ('amanda', 80)
+
+# 튜플은 언패킹이 가능하므로 다음과 같은 모습으로 구성 가능
+for name, score in zip(names, scores):
+    print(f'{name}의 점수는 {score}입니다.')
+# james의 점수는 60입니다.
+# emily의 점수는 70입니다.
+# amanda의 점수는 80입니다.
+
+print()
+for idx, name in enumerate(names):
+    print(f'{name}의 점수는 {scores[idx]}입니다.')
+
+print()
+for idx in range(len(names)):
+    print(f'{names[idx]}의 점수는 {scores[idx]}입니다.')
+
+
