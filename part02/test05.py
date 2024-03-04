3c2249f9aff70e3f523247507f3a58ffa6b539f2
junyoung <juneway@hanmail.net>
2024-03-04 17:21:31 +0900

    python study commit

--- part02/test05.py
+++ part02/test05.py
@@ -1,14 +1,8 @@
-# 부동산 실거래 파일 http://rtdown.molit.go.kr/ 다운 받아 사용.
-# 24년 update : 홈페이지 접근 후 [자료제공 바로가기] 클릭
-# 전국은 1달치, 지역을 지정하면 1년치
-# 대구 1년치로 작업
-# 계약일자는 기본 값, 파일 구분은 CSV, 나머지는 기본 값 사용.
-# 다운 받은 후 메모장에서 파일을 열고 15행까지는 삭제
-
-# 조건 : 래미안 단지만 검색
-# 출력조건 : 시군구, 단지명, 전용명적, 층, 거래금액
-# 저장 파일명: apt_2403_조건1.csv
+# 조건 : 롯데캐슬 단지만 검색
+# 출력조건 : 구(구만 나오도록 할 것), 단지명, 전용면적, 층, 거래금액
+# 저장 파일명: apt_2403_조건2.csv
 # with로 파일 처리
+# 예) 달서구, 롯데캐슬레이크, 84.91, 3, "32,100"
 
 import csv
 from typing import List
@@ -18,18 +12,18 @@ with open('./input/아파트(매매)_실거래가_20240304154528.csv', encoding=
     reader = csv.DictReader(csvfile)
     for row in reader:
         row: dict
-        if row["단지명"].find('래미안') >= 0:
+        if row["단지명"].find('롯데캐슬') >= 0:
             new_data: dict = dict()
-            new_data['시군구'] = row['시군구']
+            new_data['시군구'] = row['시군구'].split()[1]
             new_data['단지명'] = row['단지명']
             new_data['전용면적(㎡)'] = row['전용면적(㎡)']
             new_data['층'] = row['층']
             new_data['거래금액(만원)'] = row['거래금액(만원)']
             new_datas.append(new_data)
-print(new_datas)
+# print(new_datas)
 header: list = list(new_datas[0].keys())
 
-file_name = 'apt_2403_조건1.csv'
+file_name = 'apt_2403_조건2.csv'
 with open(f'./output/{file_name}', 'w', newline='') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=header)
     writer.writeheader()
